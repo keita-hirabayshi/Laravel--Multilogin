5edda6af29bf04a101be3b1ffc67dd41593cebe1
Keita <178keita1034@gmail.com>
2023-04-20 07:01:57 +0900

    スロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>test1</p>
-</body>
-</html>
+<x-tests.app>
+    <!-- appのコンポーネント内のslot枠に -->
+    コンポーネントテスト1
+</x-tests.app>
