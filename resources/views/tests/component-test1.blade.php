af8bf983f7a50cf0413e2e7eea62c6f084bbf434
Keita <178keita1034@gmail.com>
2023-04-21 07:12:56 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,7 @@
     <!-- appのコンポーネント内のslot枠に -->
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネントテスト1
-
-    <x-tests.card title="タイトル1" content="本文1" /> 
+<!-- 名前付きスロットでない属性の追加のみの場合は、属性名=値　と記載する -->
+<!-- コントローラーから変数を渡す場合には、出力元で属性と区別するために : コロンと＄マークをつける -->
+    <x-tests.card title="タイトル1" content="本文1" :message="$message" /> 
 </x-tests.app>
